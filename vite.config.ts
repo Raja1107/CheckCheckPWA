0a0989c2ab777b5b091a8cd66686ec352c61d058
Raja1107 <rk.acc@tuta.io>
2024-09-13 17:30:40 +0200

    Update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,50 +1,50 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      devOptions: {
-        enabled: true,
-      },
-      registerType: "autoUpdate",
-      injectRegister: "inline",
-      includeAssets: [
-        "./public/favicon-16x16.png",
-        "./public/apple-touch-icon.png",
-        "./public/public/favicon-32x32.png",
-      ],
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png}"],
-      },
-      manifest: {
-        name: "CheckCheckPWA",
-        short_name: "CheckCheckPWA",
-        start_url: ".",
-        display: "standalone",
-        background_color: "#18181b",
-        theme_color: "#18181b",
-        icons: [
-          {
-            src: "/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "apple-touch-icon.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
-});
+    plugins: [
+        react(),
+        VitePWA({
+            devOptions: {
+                enabled: true,
+            },
+            registerType: 'autoUpdate',
+            injectRegister: 'inline',
+            includeAssets: [
+                './public/favicon-16x16.png',
+                './public/apple-touch-icon.png',
+                './public/public/favicon-32x32.png',
+            ],
+            workbox: {
+                globPatterns: ['**/*.{js,jsx,ts,tsx,css,html,ico,png}'],
+            },
+            manifest: {
+                name: 'CheckCheckPWA',
+                short_name: 'CheckCheckPWA',
+                start_url: '.',
+                display: 'standalone',
+                background_color: '#18181b',
+                theme_color: '#18181b',
+                icons: [
+                    {
+                        src: '/android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'apple-touch-icon.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
+    ],
+})
